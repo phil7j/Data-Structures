e9bdfa9d1c2cee62d6759965d70b181958f76a56
Philip Johnson <phil7j@gmail.com>
2019-11-11 16:09:13 -0800

    finished stack and queue, woohoo

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -6,13 +6,38 @@ class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        # Add to the back
+        self.storage.add_to_head(value)
+        self.size += 1
+        return
 
     def dequeue(self):
-        pass
+        # Remove from front
+        if self.len() < 1:
+            return
+        else:
+            value = self.storage.tail.value
+            self.storage.remove_from_tail()
+            self.size -= 1
+            return value
 
     def len(self):
-        pass
+        return self.storage.length
+
+
+# test = Queue()
+# test.enqueue(5)
+# test.enqueue(25)
+# test.enqueue(15)
+# print("Size", test.size)
+# print("head", test.storage.head.value)
+# print("tail", test.storage.tail.value)
+# test.dequeue()
+# print("Size", test.size)
+# # print("Front", test.front.value)
+# # print("Back", test.back.value)
+# print("head", test.storage.head.value)
+# print("tail", test.storage.tail.value)
